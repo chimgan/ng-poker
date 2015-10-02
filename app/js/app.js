ba0efb8974baa46f6a0f805bddb56e16f4959693
Vlad <v.kovtunov@intetics.com>
2015-10-02 18:05:07 +0300

    added routing for navigation;

--- app/js/app.js
+++ app/js/app.js
@@ -9,5 +9,16 @@ var pokerApp = angular.module('ngPoker', [
 
 pokerApp.config(['$routeProvider',
   function($routeProvider) {
-
+    $routeProvider.
+      when('/home', {
+        templateUrl: 'partials/home.html',
+        controller: 'PokerHomeCtrl'
+      }).
+      when('/room', {
+        templateUrl: 'partials/room.html',
+        controller: 'PokerRoomCtrl'
+      }).
+      otherwise({
+        redirectTo: '/home'
+      })
   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,7 +4,13 @@
 
 var pokerControllers = angular.module('pokerControllers', []);
 
-pokerControllers.controller('PokerTestCtrl', ['$scope',
+pokerControllers.controller('PokerHomeCtrl', ['$scope',
+  function($scope) {
+    $scope.cards = [2,3,4,5,6];
+    $scope.game = 'Poker';
+  }]);
+
+pokerControllers.controller('PokerRoomCtrl', ['$scope',
   function($scope) {
     $scope.cards = [2,3,4,5,6];
   }]);
